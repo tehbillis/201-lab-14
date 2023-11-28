32e6255327f38488f778968e4fae84187ec085ec
Trevor Ingram <ingramtrevor10@gmail.com>
2023-11-28 08:50:10 -0800

    Added chart colors and configurations.

--- js/chart.js
+++ js/chart.js
@@ -15,11 +15,27 @@ function renderChart() {
     labels: state.allProducts.map(obj => obj.name),
     datasets: [{
       label: 'Votes',
-      data: state.allProducts.map(obj => obj.timesClicked)
+      data: state.allProducts.map(obj => obj.timesClicked),
+      backgroundColor: 'rgba(255, 99, 132)',
+      borderColor: 'transparent',
+      borderWidth: 4
     }, {
       label: 'Views',
-      data: state.allProducts.map(obj => obj.timesShown)
+      data: state.allProducts.map(obj => obj.timesShown),
+      backgroundColor: 'rgba(54, 162, 235)',
+      borderColor: 'transparent',
+      borderWidth: 4
     }]
+  }
+  const options = {
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
   }
 }
 
